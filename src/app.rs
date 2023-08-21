2ff5d4b60f75587dd079e4c85dee8a9c04a0cc94
vhespanha <vihespanha@hotmail.com>
2023-08-21 20:01:31 -0300

    hey

---
+++ src/app.rs
@@ -0,0 +1,11 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+pub fn app() -> Html {
+    html! {
+        <main>
+            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
+            <h1>{ "Clover Rifas!" }</h1>
+        </main>
+    }
+}
